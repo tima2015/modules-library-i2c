c0f045d37a7ef3fa2586a4b877f6d47d02f35b36
Тимофей Шерстобитов <woodgoldfilm@gmail.com>
2023-03-13 16:09:48 +0500

    Реализовал возвращение адресов подключенных к шине устройств

--- src/main/java/ru/funnydwarf/iot/ml/I2CModuleGroup.java
+++ src/main/java/ru/funnydwarf/iot/ml/I2CModuleGroup.java
@@ -2,9 +2,15 @@ package ru.funnydwarf.iot.ml;
 
 
 import lombok.Getter;
+import ru.funnydwarf.iot.ml.sensor.Sensor;
+import ru.funnydwarf.iot.ml.utils.I2CDriverWorker;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-public class I2CModuleGroup extends ModuleGroup {
+public class I2CModuleGroup extends ModuleGroup implements ModuleListReadable {
 
     private final String bus;
 
@@ -22,5 +28,28 @@ public class I2CModuleGroup extends ModuleGroup {
             return InitializationState.OK;
         }
         return InitializationState.INITIALIZATION_ERROR;
+    }
+
+    @Override
+    public List<Object> readModuleAdressesList() {
+        List<Object> addresses = new ArrayList<>();
+        String devicesTable = null;
+        try {
+            devicesTable = I2CDriverWorker.readDetectedDevices(bus);
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] rows = devicesTable.split("\n");
+        for (int row_i = 1; row_i < rows.length; row_i++) {
+            String addressRow = rows[row_i].substring(rows[row_i].indexOf(' ') + 1);
+            for (int column_i = 0; column_i < addressRow.length(); column_i+= 3) {
+                String address = addressRow.substring(column_i, column_i + 2);
+                if (address.equals("--") || address.equals("  ")){
+                    continue;
+                }
+                addresses.add(new I2CAddress(bus, "%h%h".formatted(row_i - 1, column_i), Sensor.class));
+            }
+        }
+        return addresses;
     }
 }
