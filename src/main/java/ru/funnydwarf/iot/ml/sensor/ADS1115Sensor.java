280e3fc05c947150fe7ca38fabfa3090b1f9eb52
Тимофей Шерстобитов <woodgoldfilm@gmail.com>
2023-04-11 12:08:29 +0500

    update

--- src/main/java/ru/funnydwarf/iot/ml/sensor/ADS1115Sensor.java
+++ src/main/java/ru/funnydwarf/iot/ml/sensor/ADS1115Sensor.java
@@ -8,16 +8,10 @@ import ru.funnydwarf.iot.ml.utils.I2CDriverWorker;
 
 @Deprecated
 public class ADS1115Sensor extends Sensor<I2CModuleGroup, I2CAddress>{
-    public ADS1115Sensor(MeasurementDescriptionRepository mdr, CurrentMeasurementSession cms, ADS1115Reader reader, I2CModuleGroup group, I2CAddress address, String name, String description) {
-        super(reader, new MeasurementDescription[]{
-                mdr.findByUnitNameAndNameAndDescription("%", "value", "value from %s".formatted(name))
-        }, cms, group, address, name, description, null);
-    }
-
-    public ADS1115Sensor(MeasurementDescriptionRepository mdr, CurrentMeasurementSession cms, String measurementName, String measurementDescription, ADS1115Reader reader, I2CModuleGroup group, I2CAddress address, String name, String description) {
-        super(reader, new MeasurementDescription[]{
-                mdr.findByUnitNameAndNameAndDescription("%", measurementName, measurementDescription)
-        }, cms, group, address, name, description, null);
+    public ADS1115Sensor(MeasurementDescriptionRepository mdr, ADS1115Reader reader, I2CModuleGroup group, I2CAddress address, String name, String description) {
+        super(reader, new MeasurementData[0]
+                //mdr.findByUnitNameAndNameAndDescription("%", "value", "value from %s".formatted(name))
+        , group, address, null, null);
     }
 
     @Override
